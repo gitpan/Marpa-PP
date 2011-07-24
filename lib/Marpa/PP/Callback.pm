bb5c165a576457fa312ac7608b9aa22d1c7a8d4c
Jeffrey Kegler <jkegl@cpan.org>
2011-07-24 17:38:54 +0000

    Import of JKEGL/Marpa-PP-0.005_008 from CPAN.
    gitpan-cpan-distribution: Marpa-PP
    gitpan-cpan-version:      0.005_008
    gitpan-cpan-path:         JKEGL/Marpa-PP-0.005_008.tar.gz
    gitpan-cpan-author:       JKEGL
    gitpan-cpan-maturity:     developer

--- lib/Marpa/PP/Callback.pm
+++ lib/Marpa/PP/Callback.pm
@@ -25,9 +25,11 @@ use English qw( -no_match_vars );
 sub Marpa::PP::location {
     Marpa::exception('No context for location callback')
         if not my $context = $Marpa::PP::Internal::CONTEXT;
-    my ( $context_type, $and_node ) = @{$context};
+    my ( $context_type, $and_node, $recce ) = @{$context};
     if ( $context_type eq 'and-node' ) {
-        return $and_node->[Marpa::PP::Internal::And_Node::START_EARLEME];
+        my $earleme = $and_node->[Marpa::PP::Internal::And_Node::START_EARLEME];
+	my $earley_sets = $recce->[Marpa::PP::Internal::Recognizer::EARLEY_SETS];
+	return $earley_sets->[$earleme]->[Marpa::PP::Internal::Earley_Set::ORDINAL];
     }
     Marpa::exception('LOCATION called outside and-node context');
 } ## end sub Marpa::PP::location
@@ -35,9 +37,11 @@ sub Marpa::PP::location {
 sub Marpa::PP::cause_location {
     Marpa::exception('No context for cause_location callback')
         if not my $context = $Marpa::PP::Internal::CONTEXT;
-    my ( $context_type, $and_node ) = @{$context};
+    my ( $context_type, $and_node, $recce ) = @{$context};
     if ( $context_type eq 'and-node' ) {
-        return $and_node->[Marpa::PP::Internal::And_Node::CAUSE_EARLEME];
+        my $earleme = $and_node->[Marpa::PP::Internal::And_Node::CAUSE_EARLEME];
+	my $earley_sets = $recce->[Marpa::PP::Internal::Recognizer::EARLEY_SETS];
+	return $earley_sets->[$earleme]->[Marpa::PP::Internal::Earley_Set::ORDINAL];
     }
     Marpa::exception('cause_location() called outside and-node context');
 } ## end sub Marpa::PP::cause_location
@@ -45,16 +49,5 @@ sub Marpa::PP::cause_location {
 no strict 'refs';
 *{'Marpa::PP::token_location'} = \&Marpa::PP::cause_location;
 use strict;
-
-sub Marpa::PP::length {
-    Marpa::exception('No context for LENGTH tie')
-        if not my $context = $Marpa::PP::Internal::CONTEXT;
-    my ( $context_type, $and_node ) = @{$context};
-    if ( $context_type eq 'and-node' ) {
-        return $and_node->[Marpa::PP::Internal::And_Node::END_EARLEME]
-            - $and_node->[Marpa::PP::Internal::And_Node::START_EARLEME];
-    }
-    Marpa::exception('LENGTH called outside and-node context');
-} ## end sub Marpa::PP::length
 
 1;
