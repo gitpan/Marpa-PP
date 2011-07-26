17c47b916e33f3342362b7502eb62e82f74d2b34
Jeffrey Kegler <jkegl@cpan.org>
2011-07-26 17:05:21 +0000

    Import of JKEGL/Marpa-PP-0.006_000 from CPAN.
    gitpan-cpan-distribution: Marpa-PP
    gitpan-cpan-version:      0.006_000
    gitpan-cpan-path:         JKEGL/Marpa-PP-0.006_000.tar.gz
    gitpan-cpan-author:       JKEGL
    gitpan-cpan-maturity:     developer

--- lib/Marpa/PP.pm
+++ lib/Marpa/PP.pm
@@ -19,7 +19,7 @@ use 5.010;
 use strict;
 use warnings;
 use vars qw($VERSION $STRING_VERSION $DEBUG);
-$VERSION = '0.005_008';
+$VERSION = '0.006_000';
 $STRING_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 $DEBUG = 0;
